05e14558075f6fc63e7f77c46f73e63e1d5169e6
sk7001 <dummy@gmail.com>
2026-03-10 12:20:56 +0000

    DAY 16

--- client/src/app/bank/types/tstypes/Transactionts.ts
+++ client/src/app/bank/types/tstypes/Transactionts.ts
@@ -1,5 +1,21 @@
-export class Transactionts {
-  
+export class TransactionTS {
+    transactionId?: number;
+    accountId: string;
+    amount: number;
+    transactionDate: Date;
+    transactionType: string = "";
 
-   
+    constructor(accountId: string, amount: number, transactionDate: Date, transactionId?: number) {
+        this.accountId = accountId;
+        this.amount = amount;
+        this.transactionDate = transactionDate;
+        this.transactionId = transactionId;
+    }
+
+    displayInfo(): void {
+        console.log(`Transaction ID: ${this.transactionId}`);
+        console.log(`Account ID: ${this.accountId}`)
+        console.log(`Amount: ${this.amount.toFixed(2)}`)
+        console.log(`Transaction Date: ${this.transactionDate}`)
+    }
 }
